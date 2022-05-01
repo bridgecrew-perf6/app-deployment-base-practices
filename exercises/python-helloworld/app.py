60b7c9474f86af22fee27bc37925947f2d7c8c7c
mpcosta <markkcosta@gmail.com>
2022-05-01 02:35:25 +0100

    Implementing Logging

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,5 +1,5 @@
 import json
-
+import logging
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,6 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    # Logging a CUSTOM message
+    app.logger.info('Main Request Successful')
     return "Hello World!"
 
 
@@ -17,6 +19,7 @@ def status():
         status=200,
         mimetype='application/json'
     )
+    app.logger.info('/status Request Successful')
     return response
 
 
@@ -27,8 +30,11 @@ def metrics():
         status=200,
         mimetype='application/json'
     )
+    app.logger.info('/metrics Request Successful')
     return response
 
 
 if __name__ == "__main__":
+    # Stream logs to a file, and set the default log level to DEBUG
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
     app.run(host='0.0.0.0')
